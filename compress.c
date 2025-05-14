3b81a14eb765bd610505860b78007dc8193e2348
octonoodle <162030697+octonoodle@users.noreply.github.com>
2025-05-14 01:44:07 -0400

    very suspicious complete qoi encoder

--- compress.c
+++ compress.c
@@ -40,13 +40,60 @@ void main_decode(FILE *outputptr) {
   struct pixel seen_pix[64];
   memset(seen_pix, 0, sizeof(seen_pix));
   struct pixel prev_pix = {0,0,0};
-  
+
   // chunk encoding loop
+  bool running = false;
   for(int pix = 0; pix < width * height; pix++) {
     struct pixel this_pix = {image[3*pix], image[3*pix + 1], image[3*pix + 2]};
     if (pix_equal(this_pix, prev_pix)) {
-      printf("hoo boy");
+      if (running) {
+	fseek(outputptr, -1, SEEK_CUR);
+	uint8_t run_so_far;
+	fread(&run_so_far, 1, 1, outputptr);
+	run_so_far = run_so_far - 128 - 64; // get rid of header
+	if (run_so_far == 61) {
+	  uint8_t mt_run = 128 + 64;
+	  fwrite(&mt_run, 1, 1, outputptr); // new run in next byte
+	} else {
+	  fseek(outputptr, -1, SEEK_CUR);
+	  uint8_t new_run = run_so_far + 128 + 64 + 1;
+	  fwrite(&new_run, 1, 1, outputptr); //increment run
+	}
+      } else {
+	running = true;
+	uint8_t mt_run = 128 + 64;
+	fwrite(&mt_run, 1, 1, outputptr); // new run in next byte
+      }
+    } else { // not same as prev pix
+      running = false;
+      uint8_t this_hash = index_hash(this_pix);
+      if (pix_equal(this_pix,seen_pix[this_hash])) { // use index
+	fwrite(&this_hash, 1, 1, outputptr);
+      } else { // not indexed
+	seen_pix[this_hash] = this_pix; // a new pixel approaches
+	int dr = this_pix.r - prev_pix.r;
+	int dg = this_pix.g - prev_pix.g;
+	int db = this_pix.b - prev_pix.b;
+	if ((dr < 2 && dr > -3) && (dg < 2 && dg > -3) && (db < 2 && db > -3)) { // use diff [-2 to 1]
+	  dr += 2;
+	  dg += 2;
+	  db += 2;
+	  uint8_t diff = 64 + 16 * dr + 4 * dg + db;
+	  fwrite(&diff, 1, 1, outputptr);
+	} else if ((dr < 8 && dr > -9) && (dg < 32 && dg > -33) && (db < 8 && db > -33)) { // use luma [-8 to 7]
+	  int dr_dg = dr - dg;
+	  int db_dg = db - dg;
+	  uint8_t luma[2] = {128 + dg + 32, (dr_dg + 8) * 16 + db_dg + 8 };
+	  fwrite(luma, 1, 2, outputptr);
+	} else { // full rgb pixel
+	  uint8_t rgb[4] = {254, this_pix.r, this_pix.g, this_pix.b};
+	  fwrite(rgb, 1, 4, outputptr);
+	}
+      }
     }
+
+    // very important!
+    prev_pix = this_pix;
   }
   
   //printf("\n");
@@ -56,7 +103,7 @@ void main_decode(FILE *outputptr) {
   //}
   
   free(image);
-  printf("done");
+  printf("done main\n");
 }
 
 int main() 
@@ -131,13 +178,20 @@ int main()
   int n = sizeof(header) / sizeof(uint8_t);
   fwrite(header, sizeof(uint8_t), n, outputptr); //header
 
+  // - - - - - - - - - - - - -
   main_decode(outputptr);
+  // - - - - - - - - - - - - -
   
   // termination
+  char end[8] = {0, 0, 0, 0, 0, 0, 0, 1};
+  fwrite(end, 1, 8, outputptr);
+  printf("done done!\n");
 
+  
   // cleanup
   fclose(inputptr);
   fclose(outputptr);
+
   
   return 0;
 }
